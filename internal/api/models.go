76238e9c8abebb78e357e9a4b61d8066725e53f4
default <nettas12@outlook.com>
2026-04-20 20:20:08 +0000

    additional go changes for lat long

--- internal/api/models.go
+++ internal/api/models.go
@@ -14,8 +14,8 @@ type SecurityEvent struct {
 	Region                        string  `json:"region"`
 	ASN                           string  `json:"asn"`
 	Network                       string  `json:"network"`
-	Latitude                      float64 `json:"latitude"`
-	Longitude                     float64 `json:"longitude"`
+	Latitude                      string  `json:"latitude"`
+	Longitude                     string  `json:"longitude"`
 	VhName                        string  `json:"vh_name"`
 	AppType                       string  `json:"app_type"`
 	App                           string  `json:"app"`
